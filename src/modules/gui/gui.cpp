7ad9a7ebaf36bea33668acd13a9f17b35cb91fc5
sheepdotcom <141453189+sheepdotcom@users.noreply.github.com>
2024-11-28 19:09:11 -0500

    oops

--- src/modules/gui/gui.cpp
+++ src/modules/gui/gui.cpp
@@ -257,16 +257,12 @@ namespace eclipse::gui {
 
         switch (type) {
             default:
-#ifndef GEODE_IS_MOBILE
             case RendererType::ImGui:
                 m_renderer = std::make_shared<imgui::ImGuiRenderer>();
                 break;
-#endif
-#ifndef GEODE_IS_DESKTOP
             case RendererType::Cocos2d:
                 m_renderer = std::make_shared<cocos::CocosRenderer>();
                 break;
-#endif
         }
 
         m_renderer->init();
